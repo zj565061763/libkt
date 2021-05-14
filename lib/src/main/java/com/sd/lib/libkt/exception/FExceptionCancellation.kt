2a0e6a1469d138d5a7394fc370735365e1c58ec9
zjun <565061763@qq.com>
2021-05-14 23:52:52 +0800

    remove result api

--- lib/src/main/java/com/sd/lib/libkt/exception/FExceptionCancellation.kt
+++
@@ -1,9 +0,0 @@
-package com.sd.lib.libkt.exception
-
-/**
- * 取消
- */
-class FExceptionCancellation : FException {
-    @JvmOverloads
-    constructor(message: String? = "", cause: Throwable? = null) : super(message, cause)
-}
--- lib/src/main/java/com/sd/lib/libkt/exception/FExceptionHttp.kt
+++
@@ -1,9 +0,0 @@
-package com.sd.lib.libkt.exception
-
-/**
- * Http异常
- */
-open class FExceptionHttp : FException {
-    @JvmOverloads
-    constructor(message: String? = "", cause: Throwable? = null) : super(message, cause)
-}
