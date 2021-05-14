1b61e900cded4b5bd0576adac592fe412acdde56
zjun <565061763@qq.com>
2021-05-14 21:12:19 +0800

    FExceptionCode

---
+++ lib/src/main/java/com/sd/lib/libkt/exception/FExceptionCode.kt
@@ -0,0 +1,13 @@
+package com.sd.lib.libkt.exception
+
+/**
+ * 状态码异常
+ */
+open class FExceptionCode : FException {
+    val code: Int
+
+    @JvmOverloads
+    constructor(code: Int, message: String? = "", cause: Throwable? = null) : super(message, cause) {
+        this.code = code
+    }
+}
--- lib/src/main/java/com/sd/lib/libkt/exception/FExceptionIllegalState.kt
+++
@@ -1,9 +0,0 @@
-package com.sd.lib.libkt.exception
-
-/**
- * 不合法的逻辑状态，通常需要UI提示
- */
-open class FExceptionIllegalState : FException {
-    @JvmOverloads
-    constructor(message: String? = "", cause: Throwable? = null) : super(message, cause)
-}
